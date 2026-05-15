f04137f5271475cba2aab713b5aae143a4f68cca
David Falk <spreetin@protonmail.com>
2026-05-15 08:06:16 +0200

    Add some more stuff to minimal

--- modules/configurations/minimal.nix
+++ modules/configurations/minimal.nix
@@ -8,10 +8,14 @@
     imports = with config.flake.modules.neovim; [
       common
       blink-cmp
+      conform
+      lint
       mini
       snacks
       treesitter
       which-key
+      lang_bash
+      lang_nix
     ];
   };
 }
