c556e1d4623cb30c6f0424476ac2b62a94da81bc
David Falk <spreetin@protonmail.com>
2026-05-15 08:31:10 +0200

    Expand small a bit

--- modules/configurations/small.nix
+++ modules/configurations/small.nix
@@ -6,21 +6,33 @@
   flake.modules.neovim."config_small" = {
     imports = with config.flake.modules.neovim; [
       common
+
+      # Plugins
       blink-cmp
       conform
       lint
       lsp
       mini
+      neotree
       snacks
       telescope
+      toggleterm
       treesitter
       trouble
       which-key
+
+      # Languages
       lang_bash
       lang_c
+      lang_css
+      lang_html
       lang_json
+      lang_markdown
       lang_nix
       lang_python
+      lang_sql
+      lang_typescript
+      lang_yaml
     ];
   };
 }
